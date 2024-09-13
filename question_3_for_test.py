57e1bb3698efe1dc149221489982b6f47bb6dde9
amirkle27 <amirkle@gmail.com>
2024-09-14 01:02:34 +0300

    Delete question_3_for_test.py

--- question_3_for_test.py
+++
@@ -1,8 +0,0 @@
-def country_year_winner_for_test (country, year):
-    sqlite_lib.connect('eurovision_db.db')
-    song_name = sqlite_lib.run_query_select(f"SELECT song_name FROM eurovision_winners ew WHERE country LIKE '{country}' AND year LIKE {year}")
-    if song_name:
-        return song_name [0][0]
-    else:
-        return "Wrong"
-    sqlite_lib.close()
